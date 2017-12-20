955d50c83d62890b11fb0e1a4373dd8f43f99624
jenoveil <34443934+jenoveil@users.noreply.github.com>
2017-12-20 05:14:33 -0500

    Add files via upload
    untested

---
+++ index.js
@@ -0,0 +1,16 @@
+const Command = require('command')
+
+module.exports = function emoteBlock(dispatch) {
+  const command = Command(dispatch);
+  let enabled = true;
+
+  command.add('blockemotes', () => {
+    enabled = !enabled;
+    command.message('Emote block ' + (enabled ? 'enabled' : 'disabled') + '.');
+  });
+
+  dispatch.hook(S_SOCIAL, 1, (event) => {
+    return false;
+  });
+
+}
